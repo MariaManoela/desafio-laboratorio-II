c6d5fddd932c8b7918fdad237e31e9e83ffb34f7
Maria Manoela <manoelamvc@hotmail.com>
2023-04-14 15:57:15 -0300

    :package: feat: add files

---
+++ src/Labirinto.java
@@ -0,0 +1,15 @@
+public class Labirinto {
+    private char[][] labirinto;
+
+    public void criaLabirinto(String fileName) {
+
+    }
+
+    public void percorreLabirinto() {
+
+    }
+
+    private boolean verificaSaidasLabirinto() {
+        return false;
+    }
+}
